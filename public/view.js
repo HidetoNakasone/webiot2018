818fb2039119b56e2f9a5d04be8fb573403df5b7
HidetoNakasone <highenjoyproject@outlook.jp>
2018-12-16 10:09:19 +0900

    re

--- public/view.js
+++ public/view.js
@@ -4,7 +4,7 @@ window.onload = function () {
   setInterval(function() {
     $.ajax({
       type: 'get',
-      url: 'https://192.168.11.30:4567/get_msg',
+      url: 'http://192.168.11.30:4567/get_msg',
       // url: 'http://192.168.0.3:4567/get_msg',
       // url: 'http://192.168.11.52:4567/get_msg',
       dataType: 'json',
